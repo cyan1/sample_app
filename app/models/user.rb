7e9326865ce624ec06c7e5beecdba579c4e1e462
Chad Yan <yan.chad@gmail.com>
2012-04-16 00:07:33 -0400

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,9 +42,14 @@ class User < ActiveRecord::Base
   #end
   
   def self.authenticate(email, submitted_password)
-      user = find_by_email(email)
-      user && user.has_password?(submitted_password) ? user : nil
-    end
+    user = find_by_email(email)
+    user && user.has_password?(submitted_password) ? user : nil
+  end
+  
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
   
   private
   
