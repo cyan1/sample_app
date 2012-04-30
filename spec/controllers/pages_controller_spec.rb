c36fb78e1d875e4c03573ee966742bfd5fa29b14
Chad Yan <yan.chad@gmail.com>
2012-04-30 02:52:25 -0400

    Finished exercises 2, 4, 5, and 6

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -18,6 +18,56 @@ describe PagesController do
       response.should have_selector("title",
                                     :content => @base_title + " | Home")
     end
+    
+    describe "microposts" do
+
+      describe "for signed in users" do
+      
+        before(:each) do
+          @user = Factory(:user)
+          test_sign_in(@user)
+        end
+
+        it "should display correct label for singular count" do
+          @user.microposts.create(:content => "first")
+          get 'home'
+          response.should have_selector("span.microposts", :content => "1 micropost")
+        end
+
+        it "should display correct label for plural count" do
+          @user.microposts.create(:content => "first")
+          @user.microposts.create(:content => "second")
+          get 'home'
+          response.should have_selector("span.microposts", :content => "2 microposts")
+        end
+
+        it "should paginate correctly" do
+          first = Factory(:micropost, :user => @user)
+          second = Factory(:micropost, :user => @user)
+          third = Factory(:micropost, :user => @user)
+
+          @user.microposts = [first, second, third]
+          30.times do
+            @user.microposts << Factory(:micropost, :user => @user)
+          end
+
+          get 'home'
+          response.should have_selector("div.pagination")
+          response.should have_selector("span.disabled", :content => "Previous")
+          response.should have_selector("a", :href => "/?page=2",
+          :content => "2")
+          response.should have_selector("a", :href => "/?page=2",
+          :content => "Next")
+        end
+      end
+      
+      describe "for non-signed in users" do
+        it "should not display delete links for posts not their own" do
+          get 'home'
+          response.should_not have_selector("a", :content => "delete")
+        end
+      end
+    end
   end
 
   describe "GET 'contact'" do
@@ -58,5 +108,4 @@ describe PagesController do
                                     :content => @base_title + " | Help")
     end
   end
-  
 end
